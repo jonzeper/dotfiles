3e0215840a0c6590c51178752302d5b0b77fb9cd
Jon Zepernick <jonzeper@gmail.com>
2013-06-27 10:05:27 -0400

    Noglob rake

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias ls='ls -al'
 alias apache2ctl='sudo /opt/local/apache2/bin/apachectl'
+
+# Allow me to do stuff like rake task[param] without complaining about brackets
+alias rake='noglob rake'
