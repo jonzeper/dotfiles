1fe34204d077835b0b10c1ff4bb82ab6387e7434
Jon Zepernick <jonzeper@gmail.com>
2020-05-05 21:38:33 -0700

    docker-compose == d-c

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,5 @@ alias apache2ctl='sudo /opt/local/apache2/bin/apachectl'
 
 # Allow me to do stuff like rake task[param] without complaining about brackets
 alias rake='noglob rake'
+
+alias d-c='docker-compose'
